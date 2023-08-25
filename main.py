9a6718e7e2a4c049891d17715e8320f2f686f9cf
Prakashgunasekaran <prakashgunasekaran@gmail.com>
2023-08-25 12:27:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,71 +6,102 @@ import feedparser
 import vertexai
 from langchain.llms import VertexAI
 from datetime import datetime
-from datetime import timedelta
 from langchain.document_loaders import WebBaseLoader
 from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import LLMChain, SequentialChain
 from langchain.prompts import PromptTemplate
 
 
+st.title("RSS News Feed Summary Generator")
 
-st.title("RSS Feed Summary Generator")
+url = st.sidebar.text_input("Enter the RSS feed url", "http://feeds.bbci.co.uk/news/rss.xml")
 
-url = st.text_input("Enter the RSS feed url","https://www.bleepingcomputer.com/feed/")
+creative_level = st.sidebar.radio("Choose creative level", ("Low", "Medium", "High"), index=0)
+clicked = st.sidebar.button("Generate Summary")
 
-clicked = st.button("Generate Summary")
-
-#date_range = st.sidebar.date_input(
-#    "Select a date range",
-#    value=(datetime.now() - timedelta(weeks=4), datetime.now())
-#)
-
-
+# Retrieve gcp service account key information from streamlit secret
 secret = st.secrets["gcp_credentials"]
 
-# Define the file path (this file will be created in the same directory as your Streamlit script)
-file_path = "service_account.json"
+service_account_key_file = "service_account.json"
 
-# Write the dictionary to this file
-with open(file_path, 'w') as f:
+# Write the service account key to the file
+with open(service_account_key_file, 'w') as f:
     json.dump(dict(secret), f)
 
 # Set the environment variable to the path of the created file
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = file_path
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_key_file
 
-
-
+model_params = {
+    "Low": {
+        "temperature": 0.1,
+        "top_p": 0.1,
+        "top_k": 1
+    },
+    "Medium": {
+        "temperature": 0.5,
+        "top_p": 0.5,
+        "top_k": 20
+    },
+    "High": {
+        "temperature": 1,
+        "top_p": 1,
+        "top_k": 40
+    }
+}
+# initialize vertex ai session
 vertexai.init(project='sada-prakash')
-llm = VertexAI(
-    model_name="text-bison@001",
-    temperature=0.2, max_output_tokens=1024, top_k=40, top_p=0.8,
-    verbose=True,
-)
 
-def get_posts(rss_feed_url):
+
+def get_summaries(rss_feed_url, creative_level):
+    """function to read the rss feed and generate summaries"""
     rss_feeds = feedparser.parse(rss_feed_url)
-    posts = []
+    temp_posts = []
     for feed in rss_feeds["entries"]:
-        post = {
-            "title": feed["title"],
-            "link": feed["link"],
-            "published_ts": feed["published"],
-            #"author": feed["author"]
-        }
-        posts.append(post)
+        if "published" in feed:
+            temp_post = {
+                "title": feed["title"],
+                "link": feed["link"],
+                "published_ts": feed["published"]
+            }
+            temp_posts.append(temp_post)
+        elif "pubDate" in feed:
+            temp_post = {
+                "title": feed["title"],
+                "link": feed["link"],
+                "published_ts": feed["pubDate"]
+            }
+            temp_posts.append(temp_post)
 
+
+    # match the published time format from the feed and sort the feeds based on published time in descending order
     try:
-        posts.sort(key=lambda x: datetime.strptime(x["published_ts"], "%a, %d %b %Y %H:%M:%S %z"), reverse=True)
+        temp_posts.sort(key=lambda x: datetime.strptime(x["published_ts"], "%a, %d %b %Y %H:%M:%S %z"), reverse=True)
     except:
-        posts.sort(key=lambda x: datetime.strptime(x["published_ts"], "%a, %d %b %Y %H:%M:%S %Z"), reverse=True)
+        temp_posts.sort(key=lambda x: datetime.strptime(x["published_ts"], "%a, %d %b %Y %H:%M:%S %Z"), reverse=True)
 
-    summaries=[]
-    for post in posts[:10]:
-        post["summary"] = generate_summary(WebBaseLoader(post["link"]).load())
-        summaries.append(post)
+    summaries = []
+
+    llm = VertexAI(
+        model_name="text-bison@001",
+        temperature=creative_level["temperature"],
+        max_output_tokens=256,
+        top_k=creative_level["top_k"],
+        top_p=creative_level["top_p"]
+    )
+    for temp_post in temp_posts[:10]:  # invoke generate_summary function for the recent 10 posts from the list
+        output = generate_summary(WebBaseLoader(temp_post["link"]).load(), llm)
+        if output:
+            summary, topic = output
+            if summary:
+                temp_post["summary"] = summary
+                temp_post["topic"] = topic
+                summaries.append(temp_post)
     return summaries
 
-def generate_summary(article):
-    prompt_template="""
+
+def generate_summary(article, llm):
+    """function to generate summary of an article using langchain"""
+    summary_prompt_template = """
 Provide a very short summary, no more than four sentences, for the following article:
 
 {text}
@@ -78,24 +109,61 @@ Provide a very short summary, no more than four sentences, for the following art
 Summary:
 
 """
-    prompt = PromptTemplate(
+    summary_prompt = PromptTemplate(
         input_variables=["text"],
-        template=prompt_template,
+        template=summary_prompt_template,
     )
-    chain = load_summarize_chain(llm,chain_type="stuff", prompt=prompt)
-    summarized_text = chain.run(article)
-    return summarized_text
 
+    summary_chain = load_summarize_chain(llm, chain_type="stuff", prompt=summary_prompt, output_key="summary")
+
+    topic_prompt_template="""Classify the following text into one of several predefined topics, such as General News, Sports, Politics, Media, Entertainment, Business, Technology, Health . Any text that cannot be clasified falls into 'General News' class\n\n
+    
+ text: If you can't afford your energy bills, you should check your direct debit, pay what you can, claim what you are entitled to and adjust your boiler.
+ class: Business
+ 
+ text: Nottinghamshire Police officer hit by train while helping man
+ class: General News
+ 
+ text:The US has sensitive nuclear technology at a nuclear power plant inside Ukraine and is warning Russia not to touch it, according to a letter the US Department of Energy sent to Russia’s state-owned nuclear energy firm Rosatom last month
+ class: Politics
+ 
+ text: Fox News settled with Dominion Voting Systems on Tuesday for $787.5 million, just hours before the case was set to go to trial
+ class: Media
+ 
+ text: {summary}
+ class: 
+"""
+    topic_prompt = PromptTemplate(
+        input_variables=["summary"],
+        template=topic_prompt_template,
+    )
+    topic_chain = LLMChain(llm=llm, prompt=topic_prompt, output_key="class")
+
+    overall_chain = SequentialChain(
+        chains=[summary_chain, topic_chain],
+        input_variables=["input_documents"],
+        output_variables=["summary", "class"],
+        verbose=True
+    )
+    try:
+        output = overall_chain(article)
+        return output["summary"], output["class"]
+    except:
+        pass
+
+
+# when the Generate Summary button is submitted
 if clicked:
     if url:
         with st.spinner("Generating summaries for the recent 10 posts..."):
-            posts = get_posts(url)
+            # invoke the function to generate summaries
+            posts = get_summaries(url, model_params[creative_level])
         if posts:
             for post in posts:
                 with st.expander(post["title"], expanded=True):
-                    st.write(f"summary:\n {post['summary']}")
-                    st.write(f"published:\n {post['published_ts']}")
-                    #st.write(f"author:\n {post['author']}")
+                    st.markdown(f"**Topic:** {post['topic']}")
+                    st.markdown(f"**Summary:** {post['summary']}")
+                    st.markdown(f"**Published:** {post['published_ts']}")
                     st.markdown(f"[link..]({post['link']})")
     else:
         st.write("Enter a rss feed url")
